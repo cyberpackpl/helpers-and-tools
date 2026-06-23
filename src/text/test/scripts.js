bf6643b3675425a1fba050bd325c9ba14bbe01fa
Adam Cyberpack <adam.lechowicz@cyberpack.pl>
2026-06-23 10:57:13 +0200

    Add new css framework:cyberpack

--- src/text/test/scripts.js
+++ src/text/test/scripts.js
@@ -1,14 +1,14 @@
-const input = document.querySelector('#count-numbers');
-const current = document.querySelector('#count-numbers');
-const currentPlace = document.querySelector('.counter_current');
-let currentLength = 0;
+// const input = document.querySelector('#count-numbers');
+// const current = document.querySelector('#count-numbers');
+// const currentPlace = document.querySelector('.counter_current');
+// let currentLength = 0;
 
 // maxLength = input.maxLength;
 
 // document.querySelector('.counter_max').innerHTML = maxLength;
 
-input.addEventListener("input", ({ currentTarget: target }) => {
-currentLength = target.value.length;
+// input.addEventListener("input", ({ currentTarget: target }) => {
+// currentLength = target.value.length;
 
 // if (currentLength >= maxLength) {
 //   document.querySelector('.counter').classList.add('max');
@@ -16,5 +16,5 @@ currentLength = target.value.length;
 //   document.querySelector('.counter').classList.remove('max');
 // }
 
-currentPlace.innerHTML = currentLength;
-});
+// currentPlace.innerHTML = currentLength;
+// });
